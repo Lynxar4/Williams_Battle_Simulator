f44943316f97b46daccb6eaf19a8429221564e22
Rafic El Badawi EL Najjar <Raficnajjar2006@gmail.com>
2026-08-28 20:14:40 +1000

    Add basic Swing window scaffold
    App.java opens a JFrame and adds an empty BattlefieldPanel as a
    starting point for the battle simulator UI.

---
+++ src/BattlefieldPanel.java
@@ -0,0 +1,9 @@
+import java.awt.Color;
+import javax.swing.JPanel;
+
+public class BattlefieldPanel extends JPanel {
+
+    public BattlefieldPanel() {
+        setBackground(new Color(235, 245, 235));
+    }
+}
